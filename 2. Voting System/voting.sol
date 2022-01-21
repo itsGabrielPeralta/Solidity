59ce81a5b39292373c0637a02c513fccf4863993
Gabriel Peralta <98152989+itsGabrielPeralta@users.noreply.github.com>
2022-01-21 17:01:05 +0100

    Update voting.sol

--- 2. Voting System/voting.sol
+++ 2. Voting System/voting.sol
@@ -1,144 +1,67 @@
 // SPDX-License-Identifier: MIT
-//pragma solidity ^0.8.0;
 pragma solidity >=0.4.4 <0.7.0;
 pragma experimental ABIEncoderV2;
 
-/*
-    Datos para probar candidatos
-    -----------------------------------
-    CANDIDATO   |   EDAD   |      ID
-    -----------------------------------
-    Toni        |    20    |    12345X
-    Alberto     |    23    |    54321T
-    Joan        |    21    |    98765P
-    Javier      |    19    |    56789W
+/*  Program to carry out an electoral process
+
+    Examples data to prove
+    ------------------------------------------------------
+    CANDIDATE   |    ID         |      AGE
+    ------------------------------------------------------
+    Antonio     |    24907468D  |      40
+    Lucas       |    54674262L  |      29
+    Javier      |    06360382P  |      61
+    Lucia       |    83897375E  |      90
+    Victoria    |    28768985X  |      33
 */
 
-contract votacion{
+contract voting{
 
-    /// --------------------------------------------------- DECLARACIÓN DE VARIABLES -------------------------------------------------------
+///     --------------------------------------------------- INITIAL PARAMETERS -------------------------------------------------------
 
-    // Dirección del propietario del contrato
+    //  Owner address variable
     address public owner;
 
-    // Constructor
+    //  Constructor
     constructor() public {
+        //  Address who execute the contract is assigned to owner variable
         owner = msg.sender;
     }
 
-    // Relacionar el nombre del candidato y el hash de sus datos personales
-    mapping(string => bytes32) id_candidato;
+    //  Mapping to link a string with a hash. The relantionship will be between the name of the candidate and the hash of their data (name, id and age)
+    mapping(string => bytes32) dataCandidate;
 
-    // Relacionar el nombre del candidato y el número de votos
-    mapping(string => uint) votos_candidato;
+    //  Mapping to link a string with a integer number. The relantionship will be between the name of the candidate and their votes
+    mapping(string => uint) votesCandidate;
 
-    // Lista para almacenar los nombres de los candidatos que se han presentado
-    string[] candidatos;
+    //  Array to storage the candidates' name 
+    string[] candidates;
 
-    // Lista del hash de la dirección de los votantes
-    bytes32[] votantes;
+    //  Array to storage the address hash of the voters
+    bytes32[] voters;
 
-    /// ------------------------------------------------------ FUNCIONES -------------------------------------------------------------------
+    //  Events
+    event CandidatePresented(string, uint, string);
 
-    /*  Función para presentarse candidato
-        Los datos personales de los candidatos se almacenan a través de su hash y se relacionan con su nombre ya que es a través de su nombre del modo en el que van a 
-        quedar almacenados en un array.
-        _nombre: nombre del candidato
-        _edad: edad del candidato
-        _id: número de identificación del candidato
+///     ------------------------------------------------------ MODIFIER & AUX FUNCTIONS -------------------------------------------------------------------
+
+    /*  Modifier to control that each address can only vote once
+        _addressVoter: address that executes the function
     */
-    function PresentarCandidato(string memory _nombre, uint _edad, string memory _id) public {
-
-        // Obtener el hash de los datos del candidato
-        bytes32 hash_candidato = keccak256(abi.encodePacked(_nombre, _edad, _id));
-
-        // Almacenar el hash de los datos del candidato que están ligados a su nombre
-        id_candidato[_nombre] = hash_candidato;
-
-        // Actualizar lista de los candidatos
-        candidatos.push(_nombre);
-    }
-
-    /*  Función para ver los candidatos que se han presentado   
-    */
-    function VerCandidatos() public view returns(string[] memory) {
-        return candidatos;
-    }
-
-    /*  Función para votar a los candidatos
-        _candidato: nombre del candidato al que vota
-    */
-    function Votar(string memory _candidato) public UnVotoPorDireccion(msg.sender){
-        // Se añade un voto al candidato seleccionado
-        votos_candidato[_candidato]++;
-        // Se incluye el hash del votante en el array de hash que ya han votado
-        bytes32 hash_votante = keccak256(abi.encodePacked(msg.sender));
-        votantes.push(hash_votante);
-    }
-
-    /* Función para ver los votos de un candidato
-        _candidato: nombre del candidato del que se quiere consultar los votos
-    */
-    function VerVotos(string memory _candidato) public view returns(uint){
-        return votos_candidato[_candidato];
-    }
-
-    /*  Función para ver los votos de cada candidato        
-    */
-    function VerResultados() public view returns(string memory){
-        
-        // Variable string que se almacenará los candidatos y sus respectivos votos
-        string memory resultados;
-
-        // Bucle para recorrer el array de candidatos e ir actualizando la variable string
-        for(uint i = 0 ; i < candidatos.length; i++){
-            resultados = string(abi.encodePacked(resultados, "(", candidatos[i], ", ", uint2str(VerVotos(candidatos[i])), ") --- "));
-        }
-    }
-
-    /*  Función para dar el ganador de la votación
-    */
-    function Ganador() public view returns(string memory){
-        // Se inicializa la variable del ganador. Se inicia con que el ganador es el de la primera posición
-        string memory ganador = candidatos[0];
-
-        // Variable para indicar si hay empate
-        bool empate;
-
-        // Se comparan los candidatos con un bucle for
-        for(uint i = 1 ; i < candidatos.length ; i++){
-            if(votos_candidato[ganador] < votos_candidato[candidatos[i]]){
-                ganador = candidatos[i];
-                empate = false;
-            }
-            else if (votos_candidato[ganador] == votos_candidato[candidatos[i]])
-                empate = true;         
-        }
-
-        if(empate)
-            ganador = "Hay un empate entre los candidatos!";
-        
-        // En caso de empate la variable ganador contendrá mensaje de texto indicando este suceso. Si no es empate contendrá el nombre del ganador
-        return ganador;
-    }
-
-    /// ------------------------------------------------------ FUNCIONES AUXILIARES -------------------------------------------------------------------
-
-    /*  Modificador para controlar que cada votante sólo pueda votar una única vez
-        Dicho control se realizará a través del hash de las direcciones
-        _direccion_votante: dirección que llama a la función
-    */
-    modifier UnVotoPorDireccion(address _direccion_votante){
-        // Se obtiene el hash de la dirección que ejecuta la función
-        bytes32 hash_votante = keccak256(abi.encodePacked(_direccion_votante));
-        // Se verifica si esta dirección ya ha votado
-        for(uint i = 0 ; i < votantes.length ; i++){
-            require(votantes[i] != hash_votante, "Ya has votado previamente");
+    modifier OneVotePerAddress(address _addressVoter){
+        // Because the stored voters data are the address' hash, the executer address' hash is obtained
+        bytes32 hash_addressVoter = keccak256(abi.encodePacked(_addressVoter));
+        // With for bucle the voters array is checked 
+        for(uint i = 0 ; i < voters.length ; i++){
+            // If the voter has voted before don't let the function execution
+            require(voters[i] != hash_addressVoter, "You can't vote because you voted once before");
         }
         _;
     }
 
-    //Funcion auxiliar que transforma un uint a un string
+    /*  Aux function that convert an integer number into string
+        _i: integer number to convert into string
+    */
     function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
         if (_i == 0) {
             return "0";
@@ -156,5 +79,84 @@ contract votacion{
             _i /= 10;
         }
         return string(bstr);
+    }
+
+///     ------------------------------------------------------ FUNCTIONS -------------------------------------------------------------------
+
+    /*  Function to present candidate
+        Los datos personales de los candidatos se almacenan a través de su hash y se relacionan con su nombre ya que es a través de su nombre del modo en el que van a 
+        quedar almacenados en un array.
+        _name: candidate name
+        _age: candidate age
+        _id: candidate ID
+    */
+    function PresentCandidate(string memory _name, uint _age, string memory _id) public {
+        // Get the hash of the candidate data
+        bytes32 hash_candidate = keccak256(abi.encodePacked(_name, _age, _id));
+        // With dataCandidate mapping the relationship between candidate name and their hash data is made
+        dataCandidate[_name] = hash_candidate;
+        // Add the candidate in the array
+        candidates.push(_name);
+    }
+
+    //  Function to visualize the presented candidates
+    function VisualizeCandidates() public view returns(string[] memory) {
+        // Just return the candidates array where their names are stored
+        return candidates;
+    }
+
+    /*  Function to vote a candidate
+        _candidateName: candidate name to vote
+    */
+    function Votar(string memory _candidateName) public OneVotePerAddress(msg.sender){
+        // A vote is add in the votes candidate mapping
+        votesCandidate[_candidateName]++;
+        // The hash of the voter's address is add in the voters array to control that this direction have voted and don't do it again
+        bytes32 hash_candidateAddress = keccak256(abi.encodePacked(msg.sender));
+        voters.push(hash_candidateAddress);
+    }
+
+    /* Function to consult a candidate votes
+        _candidateName: candidate name whose votes will be given by this function
+    */
+    function VisualizeCandidateVotes(string memory _candidateName) public view returns(uint){
+        // Just return the mapping of the candidate votes with the name introduced in the function execution
+        return votesCandidate[_candidateName];
+    }
+
+    //  Function to consult the votes of each candidate  
+    function VisualizeAllVotes() public view returns(string memory){     
+        // string variable to add each candidate and the number of his votes
+        string memory results;
+        // For bucle to consult each index in the candidates array
+        for(uint i = 0 ; i < candidates.length; i++){
+            results = string(abi.encodePacked(results, "(", candidates[i], ", ", uint2str(VisualizeCandidateVotes(candidates[i])), ") --- "));
+        }
+        return results;
+    }
+
+    //  Function to select the winner
+    function President() public view returns(string memory){
+        // Winner variable that contains the firs candidate array position
+        string memory president = candidates[0];
+        // Variable to indicate if there is a draw
+        bool draw;
+        // For bucle to compare the candidates
+        for(uint i = 1 ; i < candidates.length ; i++){
+            // If the candidate votes in the i position of candidate array is higher than the candidate stored in the president variable, the value of this variable
+            // is modified
+            if(votesCandidate[president] < votesCandidate[candidates[i]]){
+                president = candidates[i];
+                draw = false;
+            }
+            else if (votesCandidate[president] == votesCandidate[candidates[i]])
+                draw = true;         
+        }
+
+        if(draw)
+            president = "There is a draw between candiddate!";
+        
+        // In draw case the variable that returns will be a message that indicate a draw 
+        return president;
     }
 }
