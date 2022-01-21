5fc835444f221740190125086aa063a385f9eed5
Gabriel Peralta <98152989+itsGabrielPeralta@users.noreply.github.com>
2022-01-21 14:46:08 +0100

    Update evaluation.sol

--- 1. University Evaluation System/evaluation.sol
+++ 1. University Evaluation System/evaluation.sol
@@ -1,19 +1,3 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.4 < 0.7.0;
-pragma experimental ABIEncoderV2;
-
-/*  Program to evaluate a group of students
-
-    Examples data to prove
-    ------------------------------------------------------
-    STUDENTS  |    ID         |      QUALIFICATION
-    ------------------------------------------------------
-    Antonio   |    24907468D  |      7
-    Lucas     |    54674262L  |      4
-    Javier    |    06360382P  |      2
-    Lucia     |    83897375E  |      9
-    Victoria  |    28768985X  |      8
-*/
 
 contract evaluation {
     
@@ -32,7 +16,7 @@ contract evaluation {
     mapping (bytes32 => uint) qualification;
     
     //  Students can request a review. The name of the student requesting the review will be stored in the array
-    string [] student_request;
+    string [] review_request;
     
     //  Events 
     event Evaluated_Student(bytes32);
@@ -41,7 +25,7 @@ contract evaluation {
 //  -------------------------------------------------------------------- MODIFIER & AUX FUNCTIONS -----------------------------------------------------
 
     /*  Only proffessor can execute the functions that have this modifier
-        _sender: address 
+        _executingAddress: address 
     */
     modifier OnlyProffessor(address _executingAddress){
         require(_executingAddress == proffessor, "This address don't have permissions to execute this function.");
@@ -50,40 +34,43 @@ contract evaluation {
 
 //  -------------------------------------------------------------------- FUNCTIONS --------------------------------------------------------------------
 
-    // Funcion para evaluar a alumnos
-    function Evaluar(string memory _idAlumno, uint _nota) public UnicamenteProfesor(msg.sender){
-        // Hash de la identificacion del alumno 
-        bytes32 hash_idAlumno = keccak256(abi.encodePacked(_idAlumno));
-        // Relacion entre el hash de la identificacion del alumno y su nota
-        Notas[hash_idAlumno] = _nota;
-        // Emision del evento
-        emit alumno_evaluado(hash_idAlumno);
-    }
+    /*  Function to evaluate the students
+        _idStudent: ID of the student to be evaluated
+        _qualification: student qualification
+    */
+    function Evaluate(string memory _idStudent, uint _qualification) public OnlyProffessor(msg.sender){
+        // Get the hash of ID student
+        bytes32 _hash_idStudent = keccak256(abi.encodePacked(_idStudent));
+        // Link hash_idStudent with his qualification using qualification mapping
+        qualification[_hash_idStudent] = _qualification;
+        // Emits the evaluated student event
+        emit Evaluated_Student(_hash_idStudent);
+    }  
     
-
-    
-    // Funcion para ver las notas de un alumno 
-    function VerNotas(string memory _idAlumno) public view returns(uint) {
-        // Hash de la identificacion del alumno 
-        bytes32 hash_idAlumno = keccak256(abi.encodePacked(_idAlumno));
-        // Nota asociada al hash del alumno
-        uint nota_alumno = Notas[hash_idAlumno];
-        // Visualizar la nota 
-        return nota_alumno;
+    /*  Function to see students' qualification 
+        _idStudent: student's ID whose you want to consult
+    */
+    function ConsultQualification(string memory _idStudent) public view returns(uint _studentQualification) {
+        // Get the hash of ID student
+        bytes32 _hash_idStudent = keccak256(abi.encodePacked(_idStudent));
+        // Get the student's qualification using the mapping that linked the id hash with the qualification
+        _studentQualification = qualification[_hash_idStudent];
     } 
     
-    // Funcion para pedir una revision del examen
-    function Revision(string memory _idAlumno) public {
-        // Almacenamiento de la identidad del alumno en un array
-        revisiones.push(_idAlumno);
-        // Emision del evento 
-        emit evento_revision(_idAlumno);
+    /*  Function to request an exam review
+        _idStudent: ID of the student who want to be reevaluated
+    */
+    function ReviewRequest(string memory _idStudent) public {
+        // The student id requesting the review is added to the review request array
+        review_request.push(_idAlumno);
+        // Emits the event to indicate that a student has requested a review
+        emit ReviewRequest(_idStudent);
     }
     
-    // Funcion para ver los alumnos que han solicitado revision de examen
-    function VerRevisiones() public view UnicamenteProfesor(msg.sender) returns (string [] memory){
-        // Devolver las identidades de los alumnos
-        return revisiones;
+    // Function to view the students' history who have requested a review
+    function ViewReviewRequest() public view OnlyProffessor(msg.sender) returns (string [] memory) {
+        // Just return the array where the id of the students who have requested the review are add
+        return review_request;
     }
     
 }
