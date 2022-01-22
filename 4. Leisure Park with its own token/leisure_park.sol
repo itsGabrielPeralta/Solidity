7c9574f500d863d2def1302d7aac36ef08da2400
Gabriel Peralta <98152989+itsGabrielPeralta@users.noreply.github.com>
2022-01-22 12:43:50 +0100

    Update leisure_park.sol

--- 4. Leisure Park with its own token/leisure_park.sol
+++ 4. Leisure Park with its own token/leisure_park.sol
@@ -3,159 +3,182 @@ pragma solidity >=0.4.4 < 0.7.0;
 pragma experimental ABIEncoderV2;
 import "./ERC20.sol";
 
-contract Disney {
+contract leisure_park {
 
-// --------------------------------------------------------------------- DECLARACIONES INICIALES -----------------------------------------------
-    // Instancia del contrato de los token
+/// --------------------------------------------------------------------- INITIAL PARAMETERS -----------------------------------------------
+    //  Token Contract Instance Variable
     ERC20Basic private token;
 
-    // Variable de la dirección del owner. Será una dirección para pagos también
+    //  Owner Address Variable. It must be payable because this address will have ethers transfer
     address payable public owner;
 
     // Constructor
     constructor() public {
+        // Token initial supply of the contract
         token = new ERC20Basic(10000);
+        // Address who execute the contract is assigned to the owner variable
         owner = msg.sender;
     }
 
-    // Estructura de datos para almacenar a los clientes de Disney
-    struct cliente {
-        uint tokens_comprados;
-        string[] atracciones_disfrutadas;
+    // Client struct with the data needed
+    struct client {
+        uint tokens;
+        string[] rideEnjoyed;
     }
 
-    // Mapping para el registro de clientes
-    mapping(address => cliente) public Clientes;
-
-// ------------------------------------------------------------------- GESTIÓN DE TOKENS -------------------------------------------------------
-
-    // Función para establecer el precio de un token
-    function PrecioTokens(uint _numTokens) internal pure returns (uint) {
-        // Conversión de Tokens a Ethers: 1 Token -> 1 Ether
-        return _numTokens*(1 ether);
+    // Ride struct with the data needed
+    struct ride {
+        string rideName;
+        uint ridePrice;
+        bool rideState; // true is available and false not
     }
 
-    // Función para comprar tokens
-    function CompraTokens(uint _numTokens) public payable {
-        // Establecer el precio de los Tokens
-        uint coste = PrecioTokens(_numTokens);
-        // Se comprueba el dinero que el cliente paga por los tokens
-        require(msg.value >= coste, "Compra menos Tokens o pon más Ether");
-        // Diferencia de lo que el cliente paga
-        uint returnValue = msg.Value - coste;
-        // Disney retorna la cantidad de Ethers al cliente
-        msg.sender.transfer(returnValue);
-        // Obtener el balance de Tokens disponibles
-        uint balance = balanceOf();
-        require(_numTokens <= balance, "Compra un número menor de Tokens");
-        // Se transfiere el número de Tokens al cliente
-        token.transfer(msg.sender, _numTokens);
-        // Registro de tokens comprados
-        Clientes[msg.sender].tokens_comprados += _numTokens;
-    }
+    //  Mapping with address to client struct. This mapping define the relationship between an address and a client
+    mapping(address => client) public Clients;
+    // Mapping with string to ride struct. This mapping define the relationship between the ride name and a ride
+    mapping (string => ride) public Rides;
+    // Mapping with an address to string array. This mapping define the relationship between an address and the array where will be storage the ride history
+    mapping(address => string[]) RideHistory;
 
-    // Balance de tokens del contrato
-    function balanceOf() public view return(uint) {
-        // Se le pasa la dirección de este contrato al método del contrato del Token para que indique el balance
-        return token.balanceOf(address(this));
-    }
+    // String array to storage the ride's name
+    string[] RidesName;
 
-    // Función para que cada persona pueda consultar el número de tokens que tiene
-    function MisTokens() public view return(uint) {
-        return token.balanceOf(msg.sender);
-    }
+    // Events
+    event Ride_Enjoyed(string);
+    event New_Ride(string, uint);
+    event Disable_Ride(string);
 
-    // Función para generar más tokens
-    function GeneraTokens(uint _numTokens) public Unicamente(msg.sender) {
-        token.increaseTotalSuply(_numTokens);
-    }
+///  ------------------------------------------------------------------- MODIFIER & AUX FUNCTIONS -------------------------------------------------------
 
-    // Modificador para controlar las funciones ejecutables por Disney
-    modifier Unicamente(address _direccion) {
-        require(_direccion == owner, "No tienes permisos para ejecutar esta función");
+    /*  Modifier to control that the function is executed only by the owner of the contract
+        _addressExecuter: address who execute the function
+    */
+    modifier OnlyOwner(address _addressExecuter) {
+        require(_addressExecuter == owner, "This address don't have permissions to execute this function.");
         _;
     }
 
-// -------------------------------------------------------------------------- GESTIÓN DE DISNEY -----------------------------------------------------
-
-    // Eventos
-    event disfruta_atraccion(string);
-    event nueva_atraccion(string, uint);
-    event baja_atraccion(string);
-
-    // Estructura de datos de la atraccion
-    struct atraccion {
-        string nombre_atraccion;
-        uint precio_atraccion;
-        bool estado_atraccion;
+    /*  Modifier to check if a ride is available
+        _name: ride name
+    */
+    modifier RideAvailable(string memory _name) {
+        require(Rides[_name].rideState == true, "This ride is disabled rigth now.");
+        _;
     }
 
-    // Mapping para relacionar un nombre de una atracción con una estructura de datos de la atracción
-    mapping (string => atraccion) public MappingAtracciones;
 
-    // Array para almacenar el nombre de las atracciones
-    string[] Atracciones;
+///  ------------------------------------------------------------------- TOKEN MANAGEMENT -------------------------------------------------------
 
-    // Mapping para relacionar una identidad con su historial
-    mapping(address => string[]) HistorialAtracciones;
-
-    // Función para añadir atracciones
-    function NuevaAtraccion(string memory _nombreAtraccion, uint _precio) public Unicamente(msg.sender) {
-        // Creación de una atracción
-        MappingAtracciones[_nombreAtraccion] = atraccion(_nombreAtraccion, _precio, true);
-        // Almacenar en el array de atracciones el nombre
-        Atracciones.push(_nombreAtraccion);
-        // Emisión de evento para nueva atracción
-        emit nueva_atraccion(_nombreAtraccion, _precio);
+    /*  Function to define the token price
+        _numTokens: number of tokens to know their price in ether
+    */
+    function TokenPrice(uint _numTokens) internal pure returns (uint) {
+        //  1 Token -> 1 Ether
+        return _numTokens*(1 ether);
     }
 
-    // Función para dar de baja una atracción
-    function BajaAtraccion(string memory _nombreAtraccion) public Unicamente(msg.sender) {
+    /*  Function to buy tokens 
+        _numTokens: number of tokens to buy
+    */
+    function TokenBuy(uint _numTokens) public payable {
+        // Get the cost in ether 
+        uint cost = TokenPrice(_numTokens);
+        //  It is checked that the address have enough ether
+        require(msg.value >= cost, "This address don't have enough tokens.");
+        // It is checked if there is a difference between the ether sent and the cost
+        uint returnValue = msg.value - cost;
+        // Contract returns the difference value
+        msg.sender.transfer(returnValue);
+        //  Get the contract token balance
+        uint balance = ContractBalance();
+        //  It is checked that the contract have enough token to send to the buyer
+        require(_numTokens <= balance, "The contract don't have enough token. You have to buy a lower amount.");
+        //  The token is sent from the contract to the buyer's address with a ERC20.sol method
+        token.transfer(msg.sender, _numTokens);
+        //  The tokens is added to the client
+        Clients[msg.sender].tokens += _numTokens;
+    }
+
+    //  Function to query the contract token supply in this moment
+    function ContractBalance() public view returns(uint) {
+        return token.balanceOf(address(this));
+    }
+
+    //  Function to query the amount of tokens of the executing address
+    function MyTokens() public view returns(uint) {
+        return token.balanceOf(msg.sender);
+    }
+
+    /*  Function to create more tokens
+        _numTokens: amount of token to create
+    */
+    function GenerateTokens(uint _numTokens) public OnlyOwner(msg.sender) {
+        token.increaseTotalSupply(_numTokens);
+    }
+
+///  -------------------------------------------------------------------------- LEISURE PARK MANAGEMENT -----------------------------------------------------
+
+    /*  Function to add new ride
+        _name: ride name 
+        _price: ride price
+    */
+    function NewRide(string memory _name, uint _price) public OnlyOwner(msg.sender) {
+        //  Add new ride using the mapping that set the relationship between the ride name and the ride struct
+        Rides[_name] = ride(_name, _price, true);
+        // Storage in the ride name array
+        RidesName.push(_name);
+        // Emit event that indicate new ride
+        emit New_Ride(_name, _price);
+    }
+
+    /*  Function to set disabled a ride
+        _name: ride name
+    */
+    function DisableRide(string memory _name) public OnlyOwner(msg.sender) {
         // El estado de la atracción pasa a false
-        MappingAtracciones[_nombreAtraccion].estado_atraccion = false;
-        // Emisión del evento para la baja de la atracción
-        emit baja_atraccion(_nombreAtraccion);
+        Rides[_name].rideState = false;
+        // Emit the event
+        emit Disable_Ride(_name);
     }
 
-    // Función para visualizar las atracciones
-    function AtraccionesDisponibles() public view returns(string[] memory) {
-        return Atracciones;
+    //  Function to see the rides in the park 
+    function ParkRides() public view returns(string[] memory) {
+        return RidesName;
     }
 
-    // Función para subirse a una atracción y pagar en tokens
-    function SubirseAtraccion(string memory _nombreAtraccion) public {
-        // Cuanto vale la atracción en tokens
-        uint tokens_atraccion = MappingAtracciones[_nombreAtraccion].precio_atraccion;
-        // Verifica el estado de la atracción
-        require(MappingAtracciones[_nombreAtraccion].estado_atraccion == true, "La atracción no está disponible en estos momentos");
-        // Verificar el número de tokens que tiene el cliente para subirse a la atracción
-        require(tokens_atraccion <= MisTokens(), "Necesitas más tokens para subirte a esta atracción");
-        /* El cliente paga la atracción en tokens. Ha sido necesario crear una función en ERC20.sol con el nombre de transferencia_disney
-            debigo a que de haber usar el transfer o transferFrom se coge la dirección del contrato y esto no es la dirección de quien va a pagar
-        */
-        token.transferencia_disney(msg.sender, address(this), tokens_atraccion);
-        // Almacenar en el historial de las atracciones del cliente
-        HistorialAtracciones[msg.sender].push(_nombreAtraccion);
-        // Emisión del evento para disfrutar de la atracción
-        emit disfruta_atraccion(_nombreAtraccion, tokens_atraccion);
+    /*  Function to pay a ride
+        _name: ride name
+    */
+    function PayRide(string memory _name) public RideAvailable(_name) {
+        //  Get the ride price in token
+        uint _rideTokenCost = Rides[_name].ridePrice;
+        //  Check if address executer have enough tokens
+        require(_rideTokenCost <= MyTokens(), "This address don't have enough token to pay this ride.");
+        //  Use a ERC20.sol method to return the tokens from de client to the contract
+        token.returnToken(msg.sender, address(this), _rideTokenCost);
+        //  Add the ride in client history
+        RideHistory[msg.sender].push(_name);
+        // Emit the event
+        emit Ride_Enjoyed(_name);
 
     }
 
-    // Visualizar el historial completo de atracciones disfrutadas por un cliente
-    function Historial() public view returns(strings[] memory) {
-        return HistorialAtracciones[msg.sender];
+    //  Query the ride history of address who execute this function
+    function QueryRideHistory() public view returns(string[] memory) {
+        return RideHistory[msg.sender];
     }
 
-    // Función para devolver los tokens a ether
-    function DevolverTokens(uint _numTokens) public payable {
-        // El número de tokens a devolver es positivo
-        require(_numTokens > 0, "Necesitas devolver una cantidad positiva de tokens");
-        // El usuario debe de tener el número de tokens que desea devolver
-        require(_numTokens < = MisTokens(), "No tienes los tokens que deseas devolver.");
-        // El cliente devuelve los tokens
+    /*  Function to client can return token and recieve ether
+        _numTokens: amount of tokens to returns
+    */
+    function ReturnTokens(uint _numTokens) public payable {
+        //  Check if the number of tokens is higher than 0
+        require(_numTokens > 0, "You have to add a token amount higher than 0.");
+        //  Check if the cliente have enough token
+        require(_numTokens <= MyTokens(), "This address don't have enough tokens.");
+        //  The client returns tokens
         token.returnToken(msg.sender, address(this), _numTokens);
-        // Devolución de los ether al cliente
-        msg.sender.transfer(PrecioTokens(_numTokens));
+        // The contract transfer the token value in ether
+        msg.sender.transfer(TokenPrice(_numTokens));
     }
-
 }
