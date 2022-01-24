40f1963a62d9dbbb20a17c28211fa98361c34cd5
Gabriel Peralta <98152989+itsGabrielPeralta@users.noreply.github.com>
2022-01-24 18:25:46 +0100

    Create zombiefeeding.sol

---
+++ 8. Crypto Zombie/zombiefeeding.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.19;
+import "./zombiefactory.sol";
+
+//  Contract to simulate the zombie feeding. This contract inherits from the ZombieFactory contract
+contract ZombieFeeding is ZombieFactory {
+
+}
