0ff64ee13bf2ec20cd8c4e0601353cd59c32a662
Gabriel Peralta <98152989+itsGabrielPeralta@users.noreply.github.com>
2022-01-25 11:29:07 +0100

    Update zombiefeeding.sol

--- 8. Crypto Zombie/zombiefeeding.sol
+++ 8. Crypto Zombie/zombiefeeding.sol
@@ -1,8 +1,32 @@
 pragma solidity ^0.4.19;
 import "./zombiefactory.sol";
 
+//  Zombies eat CryptoKitties that is a project in Ethereum network. To use a function of CryptoKitties we set the next interface
+contract KittyInterface {
+  //  We have interest on the getKitty function inside the CryptoKitties project so it's defined below
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
+
 //  Contract to simulate the zombie feeding. This contract inherits from the ZombieFactory contract
 contract ZombieFeeding is ZombieFactory {
+
+  //  To use function in KittyInterface is necessary define a instance of her contract using its address
+  //  CryptoKitties Contract Address Variable
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  //  Define the interface instance
+  KittyInterface kittyContract = KittyInterface(ckAddress);
 
   /*  Function to feed and multiply the zombie
         _zombieId: zombie ID
@@ -21,5 +45,18 @@ contract ZombieFeeding is ZombieFactory {
     //  With the function in ZombieFactory a new zombie is created. Just for now the new zombie has no name
     _createZombie("NoName", newDna);
   }
+  
+  /*  Function to get the kitty genes using the CryptoKitty interface and simulate the zombie feeding
+      _zombieId: zombie ID
+      _kittyId: kitty ID
+  */
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    //  Kitty DNA Variable
+    uint kittyDna;
+    //  Using getKitty function to get the kitty DNA using his ID. Only has interest for this case the genes value returns
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    //  Using feedAndMultiply function that is in this contract to simulate the zombie feeding
+    feedAndMultiply(_zombieId, kittyDna);
+  }  
   
 }
