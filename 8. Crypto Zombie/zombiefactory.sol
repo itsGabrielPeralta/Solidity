0b1354dfc3a7b14dfd500234c0476a2b77074ec9
Gabriel Peralta <98152989+itsGabrielPeralta@users.noreply.github.com>
2022-01-26 17:26:09 +0100

    Update zombiefactory.sol

--- 8. Crypto Zombie/zombiefactory.sol
+++ 8. Crypto Zombie/zombiefactory.sol
@@ -20,6 +20,8 @@ contract ZombieFactory is Ownable {
         uint dna;
         uint32 level;
         uint32 readyTime;   // to limit the feeding period
+        uint16 winCount;
+        uint16 lossCount;
     }
     
     // The zombies will be storage in array so the proposal of the next variable is this. It's public so everyone can see the zombies
@@ -39,7 +41,7 @@ contract ZombieFactory is Ownable {
         //  The position in array will be de zombie ID
         //  Get the array length and subtract 1 (because the array start with index 0) to know in what position has been saved the last zombie
         //  Create and add new zombie to the array and get the ID at the same time
-        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
         // To set the zombie owner it's used the mapping defined before to set this relationship id => owner. The owner is the address who executes the function
         zombieToOwner[id] = msg.sender;
         // How this address is owner of a new zombie the count has increased in one unit
